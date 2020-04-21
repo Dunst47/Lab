1a9fc0b187fe46497ea9019192febb5520009f0b
Dunst47 <32942450+Dunst47@users.noreply.github.com>
2020-04-21 19:37:10 +0300

    Add files via upload

--- lab1.php
+++ lab1.php
@@ -1,98 +1,8 @@
-<?php
-include_once 'DBConnector.php';
-include_once 'user.php';
-include_once 'fileUploader.php';
 
-ini_set('display_errors',1);
-$conn=new DBConnector;
-if(isset($_POST['btn-save'])){
-	//$usr= new User("","","","","");
-	//$exists=$usr->isUserExist();
-	//if($exists=="false"){
-
-
-$first_name=$_POST['first_name'];
-$last_name=$_POST['last_name'];
-$city=$_POST['city_name'];
-$username=$_POST['username'];
-$password=$_POST['password'];
-$utc_timestamp=$_POST['utc_timestamp'];
-$offset=$_POST['time_zone_offset'];
-
-$file_name = $_FILES['fileToUpload']['name'];
-$file_size = $_FILES['fileToUpload']['size'];
-$file_tmp = $_FILES['fileToUpload']['tmp_name'];
-$file_type = $_FILES['fileToUpload']['type'];
-
-
-
-$user= new User($first_name,$last_name,$city,$username,$password,$utc_timestamp,$offset);
-//$uploader=new FIleUploader();
-
-if(!$user->validateForm()){
-	$user->createFormErrorSessions();
-	header("Refresh:0");
-	die();
-}
-
-$FileUpload = new FileUploader();
-            $FileUpload->setUsername($username);
-            $FileUpload->setOriginalName($file_name);
-            $FileUpload->setFileSize($file_size);
-            $FileUpload->setFileTempName($file_tmp);
-            $FileUpload->setFileType($file_type);
-           if( $FileUpload->fileWasSelected()){
-
-//$file_upload_response=$uploader->uploadFile();
-if($user->isUserExist()==false){
-	$res= $user->save();
-if($res){
-
-			    if($FileUpload-> uploadFile()){
-	echo "<br>Save Operation was Successful<br>";
-			 }
-}
-else
-{
-	echo "An Error Occured!";
-}
-}
-else
-{
-	echo "The user already exists!";
-}
-		   }
-		   else
-		   {
-			   echo "User must select a file first!";
-		   }
-//}
-//else  if ($exists=="true"){
-	//echo "User Already exists";
-//}
-//else{
-	//echo "Error";
-//}
-}/*
-if(isset($_POST['btn-display'])){
-
-$user= new User($first_name,$last_name,$city);
-$res= $user->readAll($conn->conn);
-
-if($res){
-	echo $res;
-}
-else
-{
-	echo "An Error 2 Occured!";
-}
-}*/
-//$selectAll=User::readAll($conn->conn)
-?>
 <hmtl>
 <head>
 
-<title>Title goes here
+<title>Sign up
 </title>
 <script type="text/javascript" src="validate.js"></script>
 <link rel="stylesheet" type="text/css" href="validate.css">
@@ -123,12 +33,6 @@ unset($_SESSION['form_errors']);
 </tr>
 <tr>
 <td><input type="text" name="username"  placeholder="Username"></td>
-</tr>
-<tr>
-<td><input type="password" name="password"  placeholder="Password"></td>
-</tr>
-<tr>
-<td>Profile image:<input type="file" name="fileToUpload" id="fileToUpload"  placeholder="Password"></td>
 </tr>
 <tr>
 <td><input type="text" name="city_name"  placeholder="City"></td>
